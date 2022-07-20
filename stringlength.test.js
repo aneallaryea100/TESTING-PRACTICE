9aa8db8f505c2430589ca0418757e32a08f3ddaf
Aneal Laryea <aneallaryea100@gmail.com>
2022-07-20 11:03:47 +0000

    Calculator test

--- stringlength.test.js
+++ stringlength.test.js
@@ -1,6 +1,7 @@
 const stringLength = require('./stringLength'); 
+
   
-   test('is length greater than 1', () =>{
+    test('is length greater than 1', () =>{
         expect(stringLength('hello')).toBe(5);
     });
 
@@ -12,4 +13,6 @@ const stringLength = require('./stringLength');
     test('is length more than 10', () =>{
         expect(stringLength('lewandowski')).toBe('characters must be less than 10');
     });
+  
+   
    
